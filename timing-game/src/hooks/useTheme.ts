44d0c2e577e33e4eaed2d5caaa7a9c795152eddf
Batuhan Fidan <batuhanfidan07@hotmail.com>
2025-12-01 12:15:17 +0300

    new hooks

---
+++ timing-game/src/hooks/useTheme.ts
@@ -0,0 +1,15 @@
+import { useState, useCallback } from "react";
+import { THEMES } from "../utils/constants";
+
+export const useTheme = (initialTheme = 0) => {
+  const [currentTheme, setCurrentTheme] = useState(initialTheme);
+
+  const nextTheme = useCallback(
+    () => setCurrentTheme((prev) => (prev + 1) % THEMES.length),
+    []
+  );
+
+  const theme = THEMES[currentTheme];
+
+  return { currentTheme, theme, nextTheme };
+};
