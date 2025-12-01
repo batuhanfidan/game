3f883526958d428f798b35adfd036a58b2e72caf
Batuhan Fidan <batuhanfidan07@hotmail.com>
2025-12-01 11:40:20 +0300

    error boundary added

--- timing-game/src/App.tsx
+++ timing-game/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import MainMenu from "./pages/MainMenu";
 import TwoPlayerMode from "./modes/multi/TwoPlayerMode";
@@ -5,18 +6,28 @@ import BotMode from "./modes/single/BotMode";
 import PenaltyMode from "./modes/multi/PenaltyMode";
 import SurvivalMode from "./modes/single/SurvivalMode";
 import TimeAttackMode from "./modes/single/TimeAttackMode";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { soundsReady } from "./utils/sound";
 
 function App() {
+  useEffect(() => {
+    soundsReady.then(() => {
+      console.log("All sounds loaded successfully");
+    });
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<MainMenu />} />
-      <Route path="/game/2p" element={<TwoPlayerMode />} />
-      <Route path="/game/bot" element={<BotMode />} />
-      <Route path="/game/penalty" element={<PenaltyMode />} />
-      <Route path="/game/survival" element={<SurvivalMode />} />
-      <Route path="/game/time-attack" element={<TimeAttackMode />} />{" "}
-      <Route path="*" element={<MainMenu />} />
-    </Routes>
+    <ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<MainMenu />} />
+        <Route path="/game/2p" element={<TwoPlayerMode />} />
+        <Route path="/game/bot" element={<BotMode />} />
+        <Route path="/game/penalty" element={<PenaltyMode />} />
+        <Route path="/game/survival" element={<SurvivalMode />} />
+        <Route path="/game/time-attack" element={<TimeAttackMode />} />{" "}
+        <Route path="*" element={<MainMenu />} />
+      </Routes>
+    </ErrorBoundary>
   );
 }
 
